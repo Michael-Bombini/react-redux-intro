c107256995500a05e763fc0afc718158f461ab6d
Michael Bombini <bombini.michael@gmail.com>
2022-12-12 14:19:22 +0100

    added configure store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const initialState = {
@@ -6,9 +6,8 @@ const initialState = {
   showCounter: true,
 };
 
-
 // CREATE SLICE CON REDUX TOOLKIT
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState: initialState,
   reducers: {
@@ -21,44 +20,50 @@ createSlice({
     increase(state, action) {
       state.counter += action.amount;
     },
-    toggleCounter() {
+    toggleCounter(state) {
       state.showCounter = !state.showCounter;
     },
   },
 });
 
-function counterReducer(state = initialState, action) {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  //le azioni sono il payload che passiamo quindi acceddiamo alla proprietà amount
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter + -1,
-      showCounter: state.showCounter,
-    };
-  }
+// function counterReducer(state = initialState, action) {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   //le azioni sono il payload che passiamo quindi acceddiamo alla proprietà amount
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter + -1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === "show") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
+//   if (action.type === "show") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
 
-  return state;
-}
+//   return state;
+// }
 
 // Il create store vuole passato come parametro una funzione reducer
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+
+// store con redux toolkit
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
